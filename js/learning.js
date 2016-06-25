2422026f536a4db79b237c294601f2e5e987a94b
ZennoFact <zenno04.g@gmail.com>
2016-06-26 01:38:50 +0900

    基本機能完成
    エディタ部分の見た目の強化
    章毎のデータ読み込み機能の強化（Nextボタンの完成）
    LocalStorageを使用したデータ保存機能の追加

--- js/learning.js
+++ js/learning.js
@@ -1,69 +1,120 @@
-$("a").click(function(){
-    load("description", this.getAttribute("data-index"));
+// UIパーツのサイズ設定
+var h = window.innerHeight - $('header').height()- 40;
+$('.box').height(h);
+var btnHight = $('.button-box').height();
+$('.editor').height(h - btnHight - 10);
+
+// イベントの処理
+$('a').click(function(){
+    var index = this.getAttribute("data-index");
+    load("description", index);
+    load("code", index);
+    var newIndex = parseInt(index) + 1;
+    $('#nextBtn').attr({"data-index": newIndex});
 });
 
-$(".input").blur(function(){
-    jQuery.each($(".input"), function(i, val) {
-        //$("#" + i).append(document.createTextNode(" - " + val));
-        this.classList.remove('ok');
-        if (this === val && this.innerText === data[0].answer[i]) {
-            this.classList.add('ok');
-        }
-    });
+$('#nextBtn').click(function() {
+    var index = this.getAttribute("data-index");
+    load('description', index);
+    load('code', index);
+    var newIndex = parseInt(index) + 1;
+    $('#nextBtn').attr({"data-index": newIndex});
+    $(this).prop('disabled', true);
+    $(this).removeClass('ok');
 });
 
+
+// 授業コンテンツの読み込み
 var load = function(target, fileNumber) {
     var file = data[fileNumber][target];
-    document.getElementById(target).innerHTML = convertToCode(file);
+    file = convertToCode(file);
+    // コードの場合のみ，色を付ける
+    if (target === 'code') file = textDecoration(file);
+    document.getElementById(target).innerHTML = file;
+
+    if (target === 'code') {
+        $('.input').each(function (i, elem) {
+            $(this).attr({"data-index": fileNumber + '-' + i})
+                .blur(function () {
+                    dataSave(fileNumber + '-' + i, $(this).val());
+                });
+
+            dataLoad();
+
+            // 個別の正解に合わせた回答欄の幅の調整
+            var length = data[fileNumber].answer[i].length;
+            $(this).css({"width": (length / 2 + 1)  + "em"});
+
+            if ($(this).val() === data[fileNumber].answer[i]) {
+                this.classList.add('ok');
+            }
+            elem.addEventListener("keyup", function () {
+                inputStringCheck(this, fileNumber);
+            });
+        });
+    }
+
+    $("#nextBtn").prop("disabled", true);
+    checkGoToNext();
+};
+
+ var inputStringCheck = function(self, index) {
+    jQuery.each($(".input"), function(i, elem) {
+        if (self === elem) {
+            var text = self.innerHTML;
+            if ($(':focus').length != 0) text = $(':focus').val()
+            self.classList.remove('ok');
+            if (text === data[index].answer[i]) {
+                self.classList.add('ok');
+            }
+            checkGoToNext();
+        }
+    });
+};
+
+var checkGoToNext = function() {
+    var flag = true;
+    $(".input").each(function(i, elem){
+        if (!$(this).hasClass("ok")) flag = false;
+    });
+    if (flag) {
+        $("#nextBtn").prop("disabled", false);
+        $("#nextBtn").addClass("ok");
+    } else {
+        $("#nextBtn").prop("disabled", true);
+        $("#nextBtn").removeClass("ok");
+    }
 };
 
 var convertToCode = function(str) {
-    return str.replace(/<s>/g, '<span><span class="input">').replace(/<e>/g, '</span></span>');
+    return str.replace(/<%b>/g, '<textarea class="input" rows="1"></textarea>');
 };
 
+var textDecoration = function(str) {
+    str = str.replace(/\\t/g, '<span class="blank">&nbsp;&nbsp;&nbsp;&nbsp;</span>');
+    str = str.replace(/([>\s\(\[<])(byte|short|int|long|float|double|boolean|char)([\.\s\)])/g, '$1<span class="class">$2</span>$3');
+    str = str.replace(/([>\s\(\[<])([A-Z][A-Za-z0-9]+?)([\.\s\)\[])/g, '$1<span class="class">$2</span>$3');
+    str = str.replace(/\.([A-Za-z0-9]+?)\(/g, '.<span class="method">$1</span>(');
+    str = str.replace(/(<br>|\s|\()(".*?")(\s|\)|\.|;)/g, '$1<span class="string">$2</span>$3');
+    str = str.replace(/(\s|>|\))(;|new)(\s|\.|$|<br>)/g, '$1<span class="reserved">$2</span>$3');
+    str = str.replace(/(\s|[A-Za-z0-9]|>)(=|\+|\-|\*|\/|%|\.)(\s|[A-Za-z0-9]|<)/g, '$1<span class="reserved">$2</span>$3');
+    str = str.replace(/(^|[\s>]*?)(public|private|protected|static|void|class|interface|extends|implements)(\s)/g, '$1<span class="reserved">$2</span>$3');
+    str = str.replace(/(^|>|\s)(\/\/.*?)(<br>|$)/g, '$1<span class="comment">$2</span>$3');
 
-// ここから頑張ってダブルクリックしたらtextFieldを表示できるようにする
-var inputs = Array.prototype.slice.call(document.getElementsByClassName('input'));
-inputs.forEach(function(elem) {
-    elem.addEventListener('dblclick', showTextField, false);
-});
+    return str;
+};
 
-function showTextField(e) {
-    console.log("click");
-    var item = e.target;
-    item.classList.add('none');
-    var input = document.createElement('input');
-    input.setAttribute("type", "text");
-    var regex = /Step\.\d/;
-    if (regex.test(item.innerHTML)) {
-        input.value = "";
+// データの永続化に関する処理
+var dataSave = function(key, value) {
+    if (window.localStorage) {
+        localStorage.setItem(key, value);
     } else {
-        input.value = item.innerHTML;
+        alert('ご使用されているブラウザはローカルストレージに対応していないため，データの保存はできません。')
     }
-    var parent = item.parentNode;
-    input.addEventListener("keydown", function(e) {
-        if (e.keyCode === 13 && input.value !== "") {
-            // TODO: バリデーションチェックする
-            // var classes = item.getAttribute("class").split();
-            // console.log(classes);
-            // if (classes[0] === "setting" && !isNumber(input.value)) {
-            //   return;
-            // }
-            parent.firstChild.innerHTML = input.value;
-            parent.firstChild.classList.remove("none")
-            parent.removeChild(e.target);
-            inputs.forEach(function(elem) {
-                // elem.addEventListener('mouseover', showTextField, false);
-                elem.addEventListener('dblclick', showTextField, false);
-            });
-            inputStringCheck();
-        }
+};
+var dataLoad = function() {
+    $('.input').each(function () {
+        var value = localStorage.getItem($(this).attr('data-index'));
+        if (value !== null) $(this).val(value);
     });
-    parent.appendChild(input);
-    input.focus();
-    inputs.filter(function(elem) {
-        return elem !== item;
-    }).forEach(function(elem) {
-        elem.removeEventListener('dblclick', showTextField, false);
-    });
-}
+};
